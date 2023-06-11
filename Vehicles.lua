ed276e55e152a0c1cb8b8e4ff5d39d64b05b6bac
brock6417 <126550489+brock6417@users.noreply.github.com>
2023-06-10 20:50:19 -0500

    update vehicle shop list

---
+++ Vehicles.lua
@@ -0,0 +1,10 @@
+ ['mustangbkit'] = { -- This has to match the spawn code of the vehicle
+        ['name'] = 'Mustang', -- This is the display of the vehicle
+        ['brand'] = 'Ford', -- This is the vehicle's brand (Ford, Chevrolet, BMW, Mercedes, etc)
+        ['model'] = 'mustangbkit', -- This must match the spawn code of the vehicle
+        ['price'] = 1000000, -- The price that the vehicle sells for
+        ['category'] = 'imports', -- This must match 'VehicleClass' in vehicles.meta 
+        ['categoryLabel'] = 'Imports', -- Customize for your Vehicle Shop headings (can even include spaces)
+        ['hash'] = `mustangbkit`, -- This has to match the spawn code of the vehicle and must be surrounded by backticks. Example of a Backtick: `
+        ['shop'] = 'pdm', -- Can be a single shop or multiple shops. For multiple shops for example {'shopname1','shopname2','shopname3'}
+    },
